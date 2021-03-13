cf817b586cb1b9e563d25531b31e87528a456069
Samuel Holmes <sam@edge.app>
2021-03-12 17:07:46 -0800

    createRepo implements API v3

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
+import { Cleaner } from 'cleaners'
 import { EdgeFetchFunction, EdgeLog } from 'edge-core-js'
 import nodeFetch from 'node-fetch'
 
-import { anyPromise } from './anyPromise'
+import {
+  ApiRequest,
+  ApiResponse,
+  asApiResponse,
+  asPutRepoResponse,
+  PutRepoResponse
+} from './types'
 
-const syncServers = [
-  'https://git-uk.edge.app/api/v2/store/{syncKey}',
-  'https://git-eusa.edge.app/api/v2/store/{syncKey}'
+const syncServerHostnames = [
+  'sync-us1.edge.app',
+  'sync-us2.edge.app',
+  'sync-us3.edge.app',
+  'sync-us4.edge.app',
+  'sync-eu1.edge.app',
+  'sync-eu2.edge.app',
+  'sync-eu3.edge.app',
+  'sync-eu4.edge.app'
 ]
 
-export interface CreateRepoOptions {
+export interface CommonOptions {
   fetch?: EdgeFetchFunction
   log?: EdgeLog
 }
@@ -30,20 +43,61 @@ const defaultLog: EdgeLog = Object.assign(
   }
 )
 
-export async function createRepo(
-  syncKey: string,
-  opts: CreateRepoOptions = {}
-): Promise<void> {
+async function apiRequest<T>(
+  request: ApiRequest,
+  asT: Cleaner<T>,
+  opts: CommonOptions = {}
+): Promise<ApiResponse<T>> {
   const { fetch = nodeFetch, log = defaultLog } = opts
+  const { method, url, body } = request
 
-  await anyPromise(
-    syncServers.map(async server => {
-      const start = Date.now()
-      const uri = server.replace('{syncKey}', syncKey)
-      const response = await fetch(uri, { method: 'PUT' })
-      const time = Date.now() - start
-      log(`POST ${uri} returned ${response.status} in ${time}ms`)
-      if (!response.ok) throw new Error(`Could not create repo ${syncKey}`)
-    })
+  const start = Date.now()
+  const response = await fetch(url, {
+    method,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  })
+  const timeElapsed = Date.now() - start
+
+  log(`${method} ${url} returned ${response.status} in ${timeElapsed}ms`)
+
+  if (!response.ok)
+    throw new Error(`Request ${method} ${url} failed ${response.status}`)
+
+  const responseData = asApiResponse(asT)(await response.json())
+
+  return responseData
+}
+
+export async function createRepo(
+  repoId: string,
+  opts: CommonOptions = {}
+): Promise<PutRepoResponse> {
+  let error: Error = new Error(
+    `Failed to create repo ${repoId}: no successful server response`
   )
+
+  for (let i = 0; i < syncServerHostnames.length; ++i) {
+    const hostname = syncServerHostnames[i]
+    const url = `https://${hostname}/api/v3/repo`
+
+    try {
+      const response = await apiRequest(
+        { method: 'PUT', url, body: { repoId } },
+        asPutRepoResponse,
+        opts
+      )
+
+      if (!response.success)
+        throw new Error(`Failed to create repo ${repoId}: ${response.message}`)
+
+      return response.data
+    } catch (err) {
+      error = err
+    }
+  }
+
+  throw error
 }
