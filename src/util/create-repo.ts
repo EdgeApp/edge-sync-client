bbd0046f068c8caa389425dc70f04d299c69c32b
Samuel Holmes <sam@edge.app>
2021-07-16 17:32:31 -0700

    Change to use new V2 API REST types in implementation
    Modify apiRequest to use V2 API rest types

--- src/util/create-repo.ts
+++ src/util/create-repo.ts
@@ -1,30 +1,21 @@
 import { syncServerHostnames } from '../constants'
-import { asPutRepoResponse, PutRepoResponse } from '../types'
+import { asPutStoreResponse, PutStoreResponse } from '../types/rest-types'
 import { apiRequest, CommonOptions } from './api-request'
 
 export async function createRepo(
   syncKey: string,
   opts: CommonOptions = {}
-): Promise<PutRepoResponse> {
+): Promise<PutStoreResponse> {
   let error: Error = new Error(
-    `Failed to create repo ${syncKey}: no successful server response`
+    `Failed to create repo ${syncKey}: empty sync server list`
   )
 
   for (let i = 0; i < syncServerHostnames.length; ++i) {
     const hostname = syncServerHostnames[i]
-    const url = `https://${hostname}/api/v3/repo`
+    const url = `https://${hostname}/api/v2/store/${syncKey}`
 
     try {
-      const response = await apiRequest(
-        { method: 'PUT', url, body: { syncKey } },
-        asPutRepoResponse,
-        opts
-      )
-
-      if (!response.success)
-        throw new Error(`Failed to create repo ${syncKey}: ${response.message}`)
-
-      return response.data
+      return await apiRequest({ method: 'PUT', url }, asPutStoreResponse, opts)
     } catch (err) {
       error = err
     }
